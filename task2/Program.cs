fc23efd28f3adc7f943c152c08d6ee282f58c926
«Tanya» <itsmetanya@mail.ru>
2023-04-21 19:18:08 +0300

    задача2

---
+++ task2/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0 */
+
+int [] array = GenerateArray(8);
+Console.WriteLine($"[{String.Join(", ", array)}]");
+
+int[] GenerateArray(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i ++)
+{
+    result[i] = new Random().Next(-10, 10);
+}
+return result;
+}
+
+int[] sumNeg(int[]array)
+{
+    int[]result = new int[2];
+    foreach (int el in array)
+    {
+        result[0] += el > 0 ? el : 0;
+        result[1] += el < 0 ? el : 0;
+    }
+    return result;
+}
+
+int[]r = sumNeg(array);
+Console.WriteLine($"NegativeSum = {r[1]}");
+
